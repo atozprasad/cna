31c3c5becb7a0c0ed85cb95679915c14841e4989
administrator <administrator@playground.psl.local>
2020-04-07 05:40:53 -0700

    removed older files

--- tkg/downloadBits.sh
+++
@@ -1,5 +0,0 @@
- wget http://build-squid.eng.vmware.com/build/mts/release/bora-15794451/publish/lin64/tkg_release/tanzu_tkg-cli-v0.1.1-rc.1+vmware.1/executables/tkg-linux-amd64-v0.1.1-rc.1+vmware.1.gz
- gunzip tkg-linux-amd64-v0.1.1-rc.1+vmware.1.gz
- sudo mv tkg-linux-amd64-v0.1.1-rc.1+vmware.1 /usr/local/bin/tkg
- 
- wget http://storage.googleapis.com/capv-images/release/v1.16.2/ubuntu-1804-kube-v1.16.2.ova
