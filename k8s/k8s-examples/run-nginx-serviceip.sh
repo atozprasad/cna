cd16ff8c64e6579f2ece51fc5d41ac21f3f5f2b9
atozprasad <atozcareer@gmail.com>
2020-04-19 12:30:23 -0700

    changing svc name

--- k8s/k8s-examples/run-nginx-serviceip.sh
+++ k8s/k8s-examples/run-nginx-serviceip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl create deployment --image nginx my-nginx
-kubectl expose deployment my-nginx --port=80 --type=LoadBalancer
+kubectl create deployment --image nginx my-nginx-4lb
+kubectl expose deployment my-nginx-4lb --port=80 --type=LoadBalancer
 
