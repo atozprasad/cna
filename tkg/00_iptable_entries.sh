3f47775c09fbb2d58b090e095b55ba21f4bc6ac8
atozprasad <atozcareer@gmail.com>
2020-04-24 04:49:42 -0700

    Working file

--- tkg/00_iptable_entries.sh
+++ tkg/00_iptable_entries.sh
@@ -1,6 +1,6 @@
-
-
+#!/bin/bash
 sudo sysctl -w net.ipv4.conf.ens160.route_localnet=1
 sudo iptables -t nat -I PREROUTING -p tcp -i ens160 --dport 2345 -j DNAT --to-destination 127.0.0.1:8080
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 netstat -nr
+sudo iptables -t nat -L
