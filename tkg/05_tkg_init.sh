2ca1431ddad98e4ca13766f32e5330384d37db2d
atozprasad <atozcareer@gmail.com>
2020-04-30 18:14:56 -0700

    Adding autogenerated name for the Cluster

--- tkg/05_tkg_init.sh
+++ tkg/05_tkg_init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+source govc-config.sh
+
 echo -e "\n####Invoking tkginit #######\n"
 #tkg init --ui -v 6
-tkg init --ui
+#tkg init --ui
+CLUSTERNAME="$DEPLOYMENT_NAME""-mgmt"
+echo "tkg init -i vsphere -p dev --name $CLUSTERNAME"
+tkg init -i vsphere -p dev --name $CLUSTERNAME
 
 
