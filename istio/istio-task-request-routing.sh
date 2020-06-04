f1a23e53ee5a5896c83f3c3eb3e41db5f993ec91
AtozPrasad <atozcareer@gmail.com>
2020-06-04 19:51:42 +0200

    Update istio-task-request-routing.sh

--- istio/istio-task-request-routing.sh
+++ istio/istio-task-request-routing.sh
@@ -11,16 +11,23 @@ echo -e "\n Apply a virtual service"
 cat samples/bookinfo/networking/virtual-service-all-v1.yaml
 kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
 read
+echo -e "\n NextStep..." 
 
 echo -e "\n Lets check the virtual service object"
 kubectl get virtualservices -o yaml
 read
+echo -e "\n NextStep..." 
 
 echo -e "\n And its subsets"
 kubectl get destinationrules -o yaml
 read
-
-
-
-figlet "NextStep..." 
+echo -e "\n NextStep..." 
 read
+
+echo -e "\n Test the new routing configuration"
+echo -e "\n Open the Bookinfo site in your browser. The URL is"
+http://$GATEWAY_URL/productpage
+
+
+figlet "Done, Usecase: Istio-setup " 
+
