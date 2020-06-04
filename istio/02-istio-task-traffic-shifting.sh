7f42acb217614fefaeb270639b170673a002aa84
AtozPrasad <atozcareer@gmail.com>
2020-06-04 22:40:25 +0200

    Update 02-istio-task-traffic-shifting.sh

--- istio/02-istio-task-traffic-shifting.sh
+++ istio/02-istio-task-traffic-shifting.sh
@@ -5,7 +5,7 @@ DASHBOARD_IP=192.168.20.6
 
 cd istio-1.6.0
 
-echo -e "\n "Apply destination rules"
+echo -e "\n Apply destination rules"
 read
 echo -e "\n "kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml"
 echo -e "\n "kubectl get destinationrules -o yaml"
