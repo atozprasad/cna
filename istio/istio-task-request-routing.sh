34fe7ec8e47c869227da399cda5a0e6671b08143
AtozPrasad <atozcareer@gmail.com>
2020-06-04 19:54:52 +0200

    Update istio-task-request-routing.sh

--- istio/istio-task-request-routing.sh
+++ istio/istio-task-request-routing.sh
@@ -24,9 +24,26 @@ read
 echo -e "\n NextStep..." 
 read
 
+# Setup environment variables for IngressIP and Port
+echo -e "\n Set the ingress IP and ports: (enviornment variables"
+export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+# Set GATEWAY_URL:
+echo -e "\n Set GATEWAY_URL:"
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo $GATEWAY_URL
+echo -e "\n Next Step..." 
+read
+
 echo -e "\n Test the new routing configuration"
 echo -e "\n Open the Bookinfo site in your browser. The URL is"
 http://$GATEWAY_URL/productpage
+read
+
+echo -e "\n Remove the application virtual services:"
+kubectl delete -f samples/bookinfo/networking/virtual-service-all-v1.yaml
+read
 
 
 figlet "Done, Usecase: Istio-setup " 
