ee70dc5c4e43e26a31083aafeac7918837ea4999
atozprasad <atozcareer@gmail.com>
2020-06-15 14:11:05 +0000

    uploading changes

--- tsm/deploy_acme_demo_cluster_1.sh
+++ tsm/deploy_acme_demo_cluster_1.sh
@@ -2,6 +2,10 @@ kubectl config use-context tkswlc1-admin@tkswlc1
 #git clone https://github.com/vmwarecloudadvocacy/acme_fitness_demo -b dkalani-dev3
 
 sed -i 's/catalog\.acme\.com/catalog\.acmegns\.local/g' acme_fitness_demo/kubernetes-manifests/acme_fitness_cluster1.yaml
+kubectl delete -f acme_fitness_demo/kubernetes-manifests/secrets.yaml
+kubectl delete -f acme_fitness_demo/istio-manifests/gateway.yaml
+kubectl delete -f acme_fitness_demo/kubernetes-manifests/acme_fitness_cluster1.yaml
+
 kubectl label ns default istio-injection=enabled
 kubectl apply -f acme_fitness_demo/kubernetes-manifests/secrets.yaml
 kubectl apply -f acme_fitness_demo/istio-manifests/gateway.yaml
