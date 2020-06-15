9ca3895cb2451dbb5ef85bcb107697b0ad55b3e1
atozprasad <atozcareer@gmail.com>
2020-06-15 20:55:13 +0000

    script to download ova files

---
+++ capi/vSphere/task1-download-ovafiles.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Download bits
+IMAGERPO="https://storage.googleapis.com/capv-images"
+K8SVERSION="v1.18.2"
+HAPVERSION="v1.2.4"
+K8SBASE="ubuntu-1804"
+HAPBASE="photon-3"
+K8SIMAGE=${K8SBASE}"-kube-"${K8SVERSION}".ova"
+HAPIMAGE=${HAPBASE}"-haproxy-${HAPVERSION}.ova"
+echo 
+figlet "Download OVA file"
+
+rm *.ova
+echo -e "\n Listout images available for K8S V1.18 "
+gsutil ls gs://capv-images/release/${K8SVERSION}/*
+
+echo -e "\n Listout images available for HAProxy "
+gsutil ls gs://capv-images/extra/haproxy/release/${HAPVERSION}/*.{ova,sha256} | sed 's~^gs://~http://storage.googleapis.com/~'
+echo -e "\n Downloading K8S Bits for ManagementCluster"
+echo "wget ${IMAGERPO}/"release"/${K8SVERSION}/${K8SIMAGE}"
+read
+wget ${IMAGERPO}/"release"/${K8SVERSION}/${K8SIMAGE}
+
+echo -e "\n Downloading HAProxy  Bits for ManagementCluster"
+echo -e  "\n wget ${IMAGERPO}/"extra/haproxy/release"/${HAPVERSION}/${HAPIMAGE}"
+wget ${IMAGERPO}/"extra/haproxy/release"/${HAPVERSION}/${HAPIMAGE}
+
+
+#echo -e "\n Build OVA "
+#cd cluster-api-provider-vsphere/
+#
+#git clone https://github.com/kubernetes-sigs/cluster-api-provider-vsphere.git
+echo -e "\n NextStep...";read;clear
+
+echo -e "\n setting govc environment variables"
+cat govc-config.sh
+source govc-config.sh
+
