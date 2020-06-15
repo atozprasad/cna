fe2871518e8cbcefd2cf6c35470b204a1cd2e484
atozprasad <atozcareer@gmail.com>
2020-06-15 22:44:46 +0000

    Adding Re-mark to the ova files and taking snapshot of ova

--- capi/vSphere/task1.2-upload-images.sh
+++ capi/vSphere/task1.2-upload-images.sh
@@ -7,6 +7,20 @@ govc import.spec  ${K8SIMAGE}.ova | jq ".Name=\"${K8SIMAGE}\"" | jq ".NetworkMap
 govc import.ova -options=${K8SIMAGE}.json ${K8SIMAGE}.ova
 govc object.mv /$GOVC_DATACENTER/vm/${K8SIMAGE} $VMFOLDER
 
+# Re-mark the template as a VM
+govc vm.markasvm -pool Compute-ResourcePool ${K8SIMAGE}
+# Take a snapshot of the VM
+govc snapshot.create -vm ${K8SIMAGE} root
+# Re-mark the VM as a template
+govc vm.markastemplate ${K8SIMAGE}
+
 govc import.spec ${HAPIMAGE}.ova | jq ".Name=\"${HAPIMAGE}\"" | jq ".NetworkMapping[0].Network=\"$NODE_NETWORK\""  > ${HAPIMAGE}.json
 govc import.ova -options=${HAPIMAGE}.json ${HAPIMAGE}.ova
 govc object.mv /$GOVC_DATACENTER/vm/${HAPIMAGE} $VMFOLDER
+
+# Re-mark the template as a VM
+govc vm.markasvm -pool Compute-ResourcePool ${HAPIMAGE}
+# Take a snapshot of the VM
+govc snapshot.create -vm ${HAPIMAGE} root
+# Re-mark the VM as a template
+govc vm.markastemplate ${HAPIMAGE}
