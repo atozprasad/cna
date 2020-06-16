cb0e741f25589410ac6c9ca8c4ea4b31736d06d2
atozprasad <atozcareer@gmail.com>
2020-06-16 23:07:20 +0000

    renaming the file

---
+++ capi/task1-install-bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+figlet "create kind Cluster"
+# Create a Kind Cluster
+echo -e "\n Create kind Cluster "
+kind create cluster
+# Check a cluster creation
+echo -e "\n Cluster-info"
+kubectl cluster-info
+kubectl cluster-info > ./kind-cluster.info
+
+figlet "Done with kind cluster creation"
+echo -e "\n NextStep...";read;clear
