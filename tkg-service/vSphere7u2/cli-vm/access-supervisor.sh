b6775faa8543c821c443e6906e18d17e669fd9c2
atozprasad <atozcareer@gmail.com>
2021-07-28 17:19:25 +0200

    updated to fix environment variables for supervisor login

--- tkg-service/vSphere7u2/cli-vm/access-supervisor.sh
+++ tkg-service/vSphere7u2/cli-vm/access-supervisor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source envionment.sh
-kubectl vsphere login --server https://${SUPERVISOR_CLUSTER_IP} --insecure-skip-tls-verify  -u ${PRE_USER_NAME}
+kubectl vsphere login --server https://${SUPERVISOR_CLUSTER_IP} --insecure-skip-tls-verify  -u ${SUPERVISOR_CLUSTER_USER} 
 kubectl config use-context ${VS_NAMESPACE}
