d4f579c9a143ef6cbd2c3222394edb75432ede73
atozprasad <atozcareer@gmail.com>
2021-07-29 02:20:09 +0200

    template and shell script for TKCCluster creation

--- tkg-service/vSphere7u2/cli-vm/clusters/create-tkc.sh
+++ tkg-service/vSphere7u2/cli-vm/clusters/create-tkc.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-source ../tkc-environment.sh
+source tkc-environment.sh
+rm -f create-tkc.yaml temp.yaml
+( echo "cat <<EOF >create-tkc.yaml";
+  cat create-tkc.yaml.template;
+  echo "EOF";
+) >temp.yaml
+. temp.yaml
+cat create-tkc.yaml
+exit
 kubectl apply -f create-tkc.yaml
