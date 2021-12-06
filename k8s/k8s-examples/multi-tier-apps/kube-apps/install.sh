98967f1f43b89723319f2f67356da1c56f1f05af
atozprasad <atozcareer@gmail.com>
2021-12-06 08:25:35 -0500

    Kubeapps local repo

---
+++ k8s/k8s-examples/multi-tier-apps/kube-apps/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+helm repo add bitnami https://charts.bitnami.com/bitnami
+kubectl create namespace kubeapps
+helm install kubeapps --namespace kubeapps bitnami/kubeapps
+#Creating demo credentials
+kubectl create --namespace default serviceaccount kubeapps-operator
+kubectl create clusterrolebinding kubeapps-operator --clusterrole=cluster-admin --serviceaccount=default:kubeapps-operator
+
+#Retrieve the token
+kubectl get --namespace default secret $(kubectl get --namespace default serviceaccount kubeapps-operator -o jsonpath='{range .secrets[*]}{.name}{"\n"}{end}' | grep kubeapps-operator-token) -o jsonpath='{.data.token}' -o go-template='{{.data.token | base64decode}}' && echo
+
+#Port forward
+kubectl port-forward -n kubeapps svc/kubeapps 8080:80
+
+
