1c0d912b02bc1ef1688cd7781e2f72c7a0ada842
atozprasad <atozcareer@gmail.com>
2022-08-18 20:36:07 +0000

    removed the older folder

--- tkg-service/vSphere7u3/cli-vm/vns/tkc-prasad-clu-01/create-cluster-role.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./tkc.env
-source ../vns.env
-kubectl vsphere login --server https://${SUPERVISOR_CLUSTER_IP} --insecure-skip-tls-verify  -u ${VS_NS_USER} --tanzu-kubernetes-cluster-name ${TKC_CLUSTER_NAME}
-kubectl config use-context ${TKC_CLUSTER_NAME}
-kubectl apply -f create-devops-role.yaml
