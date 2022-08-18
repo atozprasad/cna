d35fb34a0e4c1e1a81b08af74d03581799fe74ec
atozprasad <atozcareer@gmail.com>
2022-08-18 20:35:24 +0000

    renaming the folder

---
+++ tkg-service/vSphere7u3/cli-vm/vns/tkc/create-tkc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+source ../vns.env
+kubectl vsphere login --server https://${SUPERVISOR_CLUSTER_IP} --insecure-skip-tls-verify  -u ${VS_NS_USER}
+kubectl config use-context ${VS_NAMESPACE} 
+
+## Login as VSN_ADMIN
+source tkc-cluster.conf
+rm -f create-tkc.yaml temp.yaml
+( echo "cat <<EOF >create-tkc.yaml";
+  cat create-tkc.yaml.template;
+  echo "EOF";
+) >temp.yaml
+. temp.yaml
+cat create-tkc.yaml
+read -p "Press enter to apply the TKC Cluster creation yaml ... "
+kubectl apply -f create-tkc.yaml
+rm temp.yaml 
+#rm create-tkc.yaml
+
+
+
+#kubectl create clusterrolebinding default-tkg-admin-privileged-binding --clusterrole=psp:vmware-system-privileged --group=system:authenticated
+
